0ea01996f82d004cf18967b814c7617f9b575ae6
Seth Pyle <pyl13002@byui.edu>
2019-10-22 21:32:18 -0600

    work around validation layers

--- include/Engine/Renderer/Vulkan/VulkanRenderer.h
+++ include/Engine/Renderer/Vulkan/VulkanRenderer.h
@@ -1,13 +1,31 @@
 #pragma once
 
+#include <vector>
+#include <stdexcept>
+
+#include "volk.h"
+
 #include "SDL.h"
 #undef main
 
 #include "SDL_vulkan.h"
 #include "Engine/Renderer/Renderer.h"
 
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
 class VulkanRenderer {
+private:
+    void createInstance();
 public:
+    bool checkValidationLayerSupport();
     void init(const RendererParams &params);
     void beginFrame();
     void endFrame();
--- src/Engine/Renderer/Vulkan/VulkanRenderer.cpp
+++ src/Engine/Renderer/Vulkan/VulkanRenderer.cpp
@@ -1,7 +1,34 @@
 #include "Engine/Renderer/Vulkan/VulkanRenderer.h"
 
+bool VulkanRenderer::checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+    
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
 void VulkanRenderer::init(const RendererParams &params) {
-     return;
+    volkInitialize();
+    checkValidationLayerSupport();
+    createInstance();
 }
 
 void VulkanRenderer::beginFrame() {
@@ -10,4 +37,10 @@ void VulkanRenderer::beginFrame() {
 
 void VulkanRenderer::endFrame() {
     return;
+}
+
+void VulkanRenderer::createInstance() {
+    if (enableValidationLayers && !checkValidationLayerSupport()) {
+        throw std::runtime_error("validation layers unsupported");
+    }
 }
